eba1aedad924fd712843f2742742a256e57e77d7
Shalabh0698 <gbuedu@gmail.com>
2021-12-20 21:26:53 +0530

    Add files via upload

---
+++ web_project/hello/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from hello import models
+from hello.models import saveData1
+from hello import models
+
+# Register your models here.
+admin.site.register(saveData1)
